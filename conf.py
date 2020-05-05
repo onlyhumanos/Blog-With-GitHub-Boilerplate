76669b468e8413a8421884957eced56ebcaa5fab
onlyhumanos <64725480+onlyhumanos@users.noreply.github.com>
2020-05-05 18:17:12 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -3,7 +3,7 @@
 """
 
 # For Maverick
-site_prefix = "/Blog-With-GitHub-Boilerplate/"
+site_prefix = "/" #如果为其他仓库，改为"/Blog-With-GitHub-Boilerplate/"
 source_dir = "../src/"
 build_dir = "../dist/"
 index_page_size = 10
